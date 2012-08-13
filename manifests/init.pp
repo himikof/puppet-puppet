5976221fc1f0ff6b5483495a1cb4152258473c7c
Nikita Ofitserov <Nikita.Ofitserov@transas.com>
2012-08-13 14:58:20 +0400

    Create simple puppet module

---
+++ manifests/init.pp
@@ -0,0 +1,43 @@
+import 'stdlib'
+
+# Class puppet
+#
+#  Manages the puppet installation
+#
+# @author Nikita Ofitserov <himikof@gmail.com>
+# @version 1.0
+# @package puppet
+#
+class puppet (
+  $mutual_restart = true,
+) {
+
+  # Package installation
+  case $operatingsystem {
+    gentoo:
+    {
+      portage::use_flags { 'puppet':
+        package => 'app-admin/puppet',
+        use     => 'shadow sqlite3',
+      }
+      package { 'puppet':
+        ensure  => 'latest',
+        require => Portage::Use_flags['puppet'],
+      }
+      
+      service { 'puppet':
+        enable => 'true',
+      }
+      
+      if $mutual_restart {
+        # Mutual restart pattern
+        cron { 'restart-puppet':
+          command => "/etc/init.d/puppet status -q || /etc/init.d/puppet start",
+          user => 'root',
+          minute => 0,
+        }
+      }
+      
+    }
+  }
+}
